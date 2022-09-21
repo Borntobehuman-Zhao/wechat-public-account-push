ea557ca738fa1664d8a010bcd57ea6ca4ca3817e
Zhaoy0922 <55547491+Borntobehuman-Zhao@users.noreply.github.com>
2022-09-21 14:02:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "osBX553ret550QJDPAlKz8nWJdeE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "9rK5h43cpwi3dR8b7dQb1aGkPan7r4AU9KrYcfPCYcA",
       // 所在省份
       province: "陕西",
       // 所在城市
